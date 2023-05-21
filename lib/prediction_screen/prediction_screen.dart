6b26ae9df2524775adb79c6bd9b69033e072198d
Pierre Picaud <pierrepicaud385@gmail.com>
2023-05-21 11:12:45 +0300

    Init  prediction screen

---
+++ lib/prediction_screen/prediction_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PredictionScreen extends StatefulWidget {
+  const PredictionScreen({super.key});
+
+  @override
+  State<PredictionScreen> createState() => _PredictionScreenState();
+}
+
+class _PredictionScreenState extends State<PredictionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
